c063ab3f1c6dc2e21b1166912c119e7f2a68edc8
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-10-11 15:02:05 +0900

    Implement gen-token

---
+++ src/lib/token.ts
@@ -0,0 +1,31 @@
+import crypto from "crypto";
+
+// 環境変数が定義されていない場合のデフォルト値
+const DEFAULT_TOKEN_LIFETIME = 300;
+const DEFAULT_TOKEN_LENGTH = 10;
+
+// 秒単位でトークンのライフタイムを取得
+export function getTokenLifetime(): number {
+  const t = Number(process.env.TOKEN_LIFETIME);
+  if (t == null || isNaN(t)) {
+    return DEFAULT_TOKEN_LIFETIME;
+  }
+  return t;
+}
+
+// トークンの長さを取得する
+export function getTokenLength(): number {
+  const n = Number(process.env.TOKEN_LENGTH);
+  if (n == null || isNaN(n)) {
+    return DEFAULT_TOKEN_LENGTH;
+  }
+  return n;
+}
+
+// トークンを生成する
+export function CreateToken(): string {
+  const S = "abcdefghijkmnpqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ23456789";
+  return Array.from(crypto.getRandomValues(new Uint32Array(getTokenLength())))
+    .map((v) => S[v % S.length])
+    .join("");
+}
--- src/routes/authmc.ts
+++ src/routes/authmc.ts
@@ -1,7 +1,51 @@
 import express from "express";
+import { Request, Response } from "express";
+import { CreateToken, getTokenLifetime } from "../lib/token";
+import { prisma } from "../db/prisma";
 
 export const router = express.Router();
 
-router.get("/", (req, res) => {
-	res.send({ message: "Hello from authmc" });
-});
+// POST /authmc/gen-token : マインクラフト認証用のトークンを生成する
+type GenTokenReqBody = {
+  edition: string;
+  name: string;
+  mcid: string;
+};
+
+type GenTokenResBody = {
+  token: string;
+  lifetime: number;
+};
+
+router.post(
+  "/gen-token",
+  async (req: Request<GenTokenReqBody>, res: Response<GenTokenResBody>) => {
+    // TODO: 前提条件確認
+
+    // トークン生成・保存
+    const token = CreateToken();
+    const lifetime = getTokenLifetime();
+    await prisma.minecraftAuthRequest.upsert({
+      where: {
+        edition_mcid: {
+          edition: req.body.edition,
+          mcid: req.body.mcid,
+        },
+      },
+      update: {
+        token: token,
+      },
+      create: {
+        edition: req.body.edition,
+        mcid: req.body.mcid,
+        name: req.body.name,
+        token: token,
+      },
+    });
+
+    res.send({
+      token: token,
+      lifetime: lifetime,
+    });
+  }
+);
