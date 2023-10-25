ed23e4adadd733d617af220cd36582e983fa5bcf
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-10-25 20:30:10 +0900

    サーバー情報編集APIを実装

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { GenUserToken } from './routes/authmc/gen-token';
 import { AuthUserToken } from './routes/authmc/auth-token';
 import { GenServerToken } from './routes/authserver/gen-token';
 import { AuthServerToken } from './routes/authserver/auth-token';
+import { EditServerInfo } from './routes/server/edit-info';
 
 export const app = express();
 
@@ -11,12 +12,16 @@ app.use(express.urlencoded({ extended: true }));
 
 // ルーティングの設定
 // Minecraftアカウント認証API
-const authmc = express.Router();
-authmc.post('/gen-token', GenUserToken);
-authmc.post('/auth-token', AuthUserToken);
-app.use('/authmc', authmc);
+const r_authmc = express.Router();
+r_authmc.post('/gen-token', GenUserToken);
+r_authmc.post('/auth-token', AuthUserToken);
+app.use('/authmc', r_authmc);
 // Minecraftサーバー認証API
-const authserver = express.Router();
-authserver.post('/gen-token', GenServerToken);
-authserver.post('/auth-token', AuthServerToken);
-app.use('/authserver', authserver);
+const r_authserver = express.Router();
+r_authserver.post('/gen-token', GenServerToken);
+r_authserver.post('/auth-token', AuthServerToken);
+app.use('/authserver', r_authserver);
+// サーバー操作系API
+const r_server = express.Router();
+r_server.post('/edit-info', EditServerInfo);
+app.use('/server', r_server);
---
+++ src/routes/server/edit-info.ts
@@ -0,0 +1,89 @@
+import { Request, Response } from 'express';
+import { ExResponse } from '../../types/response';
+import { CreateToken, getTokenLifetime } from '../../lib/token';
+import { prisma } from '../../db/prisma';
+import { getElapsedTime } from '../../lib/date';
+import { ping } from 'bedrock-protocol';
+import {
+  PrismaClientInitializationError,
+  PrismaClientKnownRequestError,
+  PrismaClientRustPanicError,
+  PrismaClientUnknownRequestError,
+  PrismaClientValidationError,
+} from '@prisma/client/runtime/library';
+import { error } from 'console';
+import { isTypeOfOne } from '../../lib/utils';
+import { isPrismaError } from '../../lib/db';
+import { z } from 'zod';
+
+// POST /server/edit-info : Minecraftサーバーの登録情報を変更する
+
+// リクエストボディの型定義
+const EditServerInfoReqBody = z.object({
+  id: z.number(),
+  name: z.string(),
+  published: z.boolean(),
+  showIP: z.boolean(),
+});
+type EditServerInfoReqBodyType = z.infer<typeof EditServerInfoReqBody>;
+
+// レスポンスボディの型定義
+const EditServerInfoResBody = z.object({});
+type EditServerInfoResBodyType = z.infer<typeof EditServerInfoResBody>;
+
+export async function EditServerInfo(
+  req: Request<EditServerInfoReqBodyType>,
+  res: ExResponse<EditServerInfoResBodyType>
+) {
+  try {
+    // プロパティが揃っているかを確認する
+    EditServerInfoReqBody.parse(req.body);
+    // サーバーが存在するかどうか確認
+    const server = await prisma.minecraftServer.findFirst({
+      where: {
+        id: req.body.id,
+      },
+    });
+    if (server == null) {
+      res.status(404).json({
+        message: 'サーバーが存在しません',
+      });
+      return;
+    }
+    await prisma.minecraftServer.update({
+      where: {
+        id: req.body.id,
+      },
+      data: {
+        name: req.body.name,
+        published: req.body.published,
+        showIP: req.body.showIP,
+      },
+    });
+    res.status(201).json({});
+    return;
+  } catch (e) {
+    // TODO リファクタリング
+    if (!(e instanceof Error)) {
+      res.status(404).json({
+        message: '未定義のエラー',
+      });
+      console.log(e);
+    } else if (e instanceof z.ZodError) {
+      res.status(404).json({
+        message: 'リクエストパラメータが不正です',
+      });
+    } else if (isPrismaError(e)) {
+      res.status(404).json({
+        message: 'データベースエラー',
+      });
+      console.log(e);
+    } else {
+      res.status(404).json({
+        message: `未定義のエラー: ${e.name}`,
+      });
+      console.log(e);
+    }
+    return;
+  }
+}
